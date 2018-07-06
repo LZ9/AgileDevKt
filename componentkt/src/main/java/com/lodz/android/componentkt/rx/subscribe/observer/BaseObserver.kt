8b3577b587ba85f0736a4ba22115efc812154431
LZ9 <lz.mms@foxmail.com>
2018-07-06 10:11:19 +0800

    增加Rx订阅者封装

---
+++ componentkt/src/main/java/com/lodz/android/componentkt/rx/subscribe/observer/BaseObserver.kt
@@ -0,0 +1,76 @@
+package com.lodz.android.componentkt.rx.subscribe.observer
+
+import com.lodz.android.componentkt.base.application.BaseApplication
+import com.lodz.android.corekt.log.PrintLog
+import com.lodz.android.corekt.utils.getMetaData
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+
+/**
+ * 基类订阅者（无背压）
+ * Created by zhouL on 2018/7/5.
+ */
+abstract class BaseObserver<T> : Observer<T> {
+
+    private val ERROR_TAG = "error_tag"
+
+    private var mDisposable: Disposable? = null
+
+    final override fun onSubscribe(d: Disposable) {
+        mDisposable = d
+        onBaseSubscribe(d)
+    }
+
+    final override fun onNext(t: T) {
+        onBaseNext(t)
+    }
+
+    final override fun onError(t: Throwable) {
+        t.printStackTrace()
+        printTagLog(t)
+        onBaseError(t)
+    }
+
+    /** 打印标签日志 */
+    private fun printTagLog(t: Throwable) {
+        if (BaseApplication.get() == null) {
+            return
+        }
+        val tag = BaseApplication.get()!!.getMetaData(ERROR_TAG)
+        if (tag != null && tag is String) {
+            if (!tag.isEmpty()) {
+                PrintLog.e(tag, t.toString(), t)
+            }
+        }
+    }
+
+    final override fun onComplete() {
+        onBaseComplete()
+    }
+
+    fun getDisposable(): Disposable? {
+        return mDisposable
+    }
+
+    fun clearDisposable() {
+        mDisposable = null
+    }
+
+    /** 停止订阅 */
+    fun dispose() {
+        if (mDisposable != null) {
+            mDisposable!!.dispose()
+            onDispose()
+        }
+    }
+
+    open fun onBaseSubscribe(d: Disposable) {}
+
+    abstract fun onBaseNext(t: T)
+
+    abstract fun onBaseError(e: Throwable)
+
+    open fun onBaseComplete() {}
+    /** 取消订阅时回调 */
+    open fun onDispose() {}
+}
---
+++ componentkt/src/main/java/com/lodz/android/componentkt/rx/subscribe/observer/ProgressObserver.kt
@@ -0,0 +1,145 @@
+package com.lodz.android.componentkt.rx.subscribe.observer
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.support.annotation.StringRes
+import android.support.v7.app.AlertDialog
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import com.lodz.android.componentkt.R
+import com.lodz.android.corekt.utils.UiHandler
+import io.reactivex.disposables.Disposable
+
+/**
+ * 展示加载框的订阅者（无背压）
+ * Created by zhouL on 2018/7/5.
+ */
+abstract class ProgressObserver<T> : RxObserver<T>() {
+
+    final override fun onRxSubscribe(d: Disposable) {
+        showProgress()
+        onPgSubscribe(d)
+    }
+
+    final override fun onRxNext(t: T) {
+        onPgNext(t)
+    }
+
+    final override fun onRxError(e: Throwable, isNetwork: Boolean) {
+        onPgError(e, isNetwork)
+    }
+
+    final override fun onRxComplete() {
+        dismissProgress()
+        onPgComplete()
+    }
+
+    /** 加载框 */
+    var mProgressDialog: AlertDialog? = null
+
+    /** 创建默认加载框 */
+    fun create(context: Context): ProgressObserver<T> = create(context, "", true)
+
+    /** 创建加载框，配置取消参数[cancelable] */
+    fun create(context: Context, cancelable: Boolean): ProgressObserver<T> = create(context, "", cancelable)
+
+    /** 创建加载框，配置提示文字资源[strResId]和取消参数[cancelable] */
+    fun create(context: Context, @StringRes strResId: Int, cancelable: Boolean): ProgressObserver<T> = create(context, context.getString(strResId), cancelable)
+
+    /** 创建自定义加载框[dialog] */
+    fun create(dialog: AlertDialog): ProgressObserver<T> {
+        try {
+            mProgressDialog = dialog
+            mProgressDialog!!.setOnCancelListener {
+                cancelDialog()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return this
+    }
+
+    /** 创建加载框，配置提示文字[msg]和取消参数[cancelable] */
+    fun create(context: Context, msg: String, cancelable: Boolean): ProgressObserver<T> {
+        try {
+            mProgressDialog = getProgressDialog(context, msg, cancelable)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return this
+    }
+
+    /** 获取一个加载框 */
+    @SuppressLint("InflateParams")
+    private fun getProgressDialog(context: Context, msg: String, cancelable: Boolean): AlertDialog {
+        val view = LayoutInflater.from(context).inflate(R.layout.componentkt_view_progress_layout, null)
+        val progressDialog = AlertDialog.Builder(context, R.style.ProgressStyle)
+                .setView(view)
+                .create()
+        if (!msg.isEmpty()) {
+            val msgTv = view.findViewById<TextView>(R.id.msg)
+            msgTv.visibility = View.VISIBLE
+            msgTv.text = msg
+        }
+        progressDialog.setCanceledOnTouchOutside(false)
+        progressDialog.setCancelable(cancelable)
+        progressDialog.setOnCancelListener {
+            cancelDialog()
+        }
+        return progressDialog
+    }
+
+    /** 取消加载框 */
+    private fun cancelDialog() {// 用户关闭
+        dispose()
+        onPgCancel()
+        dismissProgress()
+    }
+
+    /** 显示加载框 */
+    private fun showProgress() {
+        UiHandler.post(Runnable {
+            try {
+                if (mProgressDialog != null) {
+                    mProgressDialog!!.show()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        })
+    }
+
+    /** 关闭加载框 */
+    private fun dismissProgress() {
+        UiHandler.post(Runnable {
+            try {
+                if (mProgressDialog != null) {
+                    mProgressDialog!!.dismiss()
+                    mProgressDialog = null
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        })
+    }
+
+    override fun onErrorEnd() {// 抛异常关闭
+        dismissProgress()
+    }
+
+    override fun onDispose() {// 开发者取消
+        dismissProgress()
+    }
+
+    open fun onPgSubscribe(d: Disposable) {}
+
+    abstract fun onPgNext(t: T)
+
+    abstract fun onPgError(e: Throwable, isNetwork: Boolean)
+
+    open fun onPgComplete() {}
+
+    /** 用户取消回调 */
+    open fun onPgCancel() {}
+}
