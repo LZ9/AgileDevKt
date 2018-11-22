36d8123ce95fb4c440bb37e85a4e7642bf3854e2
LZ9 <lz.mms@foxmail.com>
2018-11-22 17:48:27 +0800

    添加RV装饰器全家桶

---
+++ componentkt/src/main/java/com/lodz/android/componentkt/widget/rv/decoration/BaseSectionItemDecoration.kt
@@ -0,0 +1,128 @@
+package com.lodz.android.componentkt.widget.rv.decoration
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.text.TextPaint
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.annotation.FloatRange
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.lodz.android.corekt.anko.dp2px
+import com.lodz.android.corekt.anko.getColorCompat
+import com.lodz.android.corekt.anko.sp2px
+
+/**
+ * 分组标签基类
+ * Created by zhouL on 2018/11/22.
+ */
+open class BaseSectionItemDecoration(context: Context) : BaseItemDecoration(context) {
+
+    /** 默认文字大小  */
+    protected val DEFAULT_TEXT_SIZE_SP = 20f
+    /** 默认分组高度  */
+    protected val DEFAULT_SECTION_HEIGHT_DP = 32
+
+    /** 文字画笔  */
+    private val mTextPaint: TextPaint
+    /** 背景画笔  */
+    private val mBgPaint: Paint
+    /** 分组高度  */
+    protected var mSectionHeightPx = 0
+    /** 文字左侧间距  */
+    private var mTextPaddingLeftDp = 0
+
+    init {
+        mBgPaint = Paint()
+        mBgPaint.color = Color.GRAY
+
+        mTextPaint = TextPaint()
+        mTextPaint.color = Color.BLACK
+        mTextPaint.typeface = Typeface.DEFAULT_BOLD
+        mTextPaint.isAntiAlias = true
+        mTextPaint.isDither = true
+        mTextPaint.textSize = getContext().sp2px(DEFAULT_TEXT_SIZE_SP)
+        mTextPaint.textAlign = Paint.Align.LEFT
+
+        mSectionHeightPx = getContext().dp2px(DEFAULT_SECTION_HEIGHT_DP).toInt()
+    }
+
+    /** 设置分组的高度[height]（单位dp） */
+    fun setSectionHeight(height: Int): BaseSectionItemDecoration {
+        mSectionHeightPx = getContext().dp2px(height).toInt()
+        return this
+    }
+
+    /** 设置文字左侧间距[paddingLeft]（单位dp） */
+    fun setSectionTextPaddingLeftDp(paddingLeft: Int): BaseSectionItemDecoration {
+        mTextPaddingLeftDp = getContext().dp2px(paddingLeft).toInt()
+        return this
+    }
+
+    /** 设置文字的大小[textSize]（单位sp） */
+    fun setSectionTextSize(@FloatRange(from = 1.0) textSize: Float): BaseSectionItemDecoration {
+        mTextPaint.textSize = getContext().sp2px(if (textSize <= 0) DEFAULT_TEXT_SIZE_SP else textSize)
+        return this
+    }
+
+    /** 设置文字的字体样式[typeface]（例如：Typeface.DEFAULT、Typeface.DEFAULT_BOLD等等） */
+    fun setSectionTextTypeface(typeface: Typeface): BaseSectionItemDecoration {
+        mTextPaint.typeface = typeface
+        return this
+    }
+
+    /** 设置分组文字颜色[color] */
+    fun setSectionTextColorRes(@ColorRes color: Int): BaseSectionItemDecoration {
+        mTextPaint.color = getContext().getColorCompat(color)
+        return this
+    }
+
+    /** 设置分组文字颜色[color] */
+    fun setSectionTextColorInt(@ColorInt color: Int): BaseSectionItemDecoration {
+        mTextPaint.color = color
+        return this
+    }
+
+    /** 设置分组背景颜色[color] */
+    fun setSectionBgColorRes(@ColorRes color: Int): BaseSectionItemDecoration {
+        mBgPaint.color = getContext().getColorCompat(color)
+        return this
+    }
+
+    /** 设置分组背景颜色[color] */
+    fun setSectionBgColorInt(@ColorInt color: Int): BaseSectionItemDecoration {
+        mBgPaint.color = color
+        return this
+    }
+
+    /** 画背景，画布[canvas]，左侧坐标[left]，顶部坐标[top]，右侧坐标[right]，底部坐标[bottom] */
+    protected fun drawBgPaint(canvas: Canvas, left: Int, top: Int, right: Int, bottom: Int) {
+        canvas.drawRect(checkValue(left).toFloat(), checkValue(top).toFloat(), checkValue(right).toFloat(), checkValue(bottom).toFloat(), mBgPaint)//绘制背景色
+    }
+
+    /** 画文字，画布[canvas]，文字[text]，左侧坐标[left]，顶部坐标[top]，右侧坐标[right]，底部坐标[bottom] */
+    protected open fun drawTextPaint(canvas: Canvas, text: String, left: Int, top: Int, right: Int, bottom: Int) {
+        val fontMetrics = mTextPaint.fontMetrics
+        val baseline = (bottom + top - fontMetrics.bottom - fontMetrics.top) / 2//文字居中基线
+        canvas.drawText(text, checkValue((left + mTextPaddingLeftDp).toFloat()), checkValue(baseline), mTextPaint)//绘制文本
+    }
+
+    /** 是否是垂直列表 */
+    protected fun isVerLinearLayout(parent: RecyclerView): Boolean {
+        val layoutManager = parent.layoutManager
+        if (layoutManager == null) {
+            return false
+        }
+        if (layoutManager is GridLayoutManager) {//GridLayoutManager继承LinearLayoutManager需要先判断
+            return false
+        }
+        if (layoutManager is LinearLayoutManager) {
+            return layoutManager.orientation == RecyclerView.VERTICAL
+        }
+        return false
+    }
+}
---
+++ componentkt/src/main/java/com/lodz/android/componentkt/widget/rv/decoration/StickyItemDecoration.kt
@@ -0,0 +1,83 @@
+package com.lodz.android.componentkt.widget.rv.decoration
+
+import android.content.Context
+import android.graphics.Canvas
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * 粘黏标签装饰器（粘黏标签的文字由item里字符串的第一位截取）
+ * 泛型T可以为String或者实现了Groupable的任意类
+ * Created by zhouL on 2018/11/22.
+ */
+class StickyItemDecoration<T> private constructor(context: Context) : SectionItemDecoration<T>(context) {
+
+    companion object {
+        /** 创建 */
+        fun <T> create(context: Context): StickyItemDecoration<T> = StickyItemDecoration(context)
+    }
+
+    override fun onDraw(canvas: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+    }
+
+    override fun onDrawOver(canvas: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDrawOver(canvas, parent, state)
+        if (!isVerLinearLayout(parent)) {
+            return
+        }
+        if (mOnSectionCallback == null) {
+            return
+        }
+        val childCount = parent.childCount
+        if (childCount == 0) {
+            return
+        }
+
+        val itemCount = state.itemCount
+
+        val left = parent.paddingLeft
+        val right = parent.width - parent.paddingRight
+        for (i in 0 until childCount) {
+            val view = parent.getChildAt(i)
+            val position = parent.getChildAdapterPosition(view)
+            if (getItem(position, mOnSectionCallback!!).isEmpty()) {
+                continue
+            }
+
+            val top = parent.top
+            val bottom = parent.top + mSectionHeightPx
+
+            val sectionTop = Math.max(top, view.top - mSectionHeightPx)// 顶部section上坐标，取top和viewtop最大值
+            val sectionBottom = Math.max(bottom, view.top)// 顶部section下坐标，取bottom和viewbottom的最大值
+
+            if (isFirstGroupItem(position, mOnSectionCallback!!)) {// 分组的第一个数据，绘制分组样式
+                drawBgPaint(canvas, left, sectionTop, right, sectionBottom)
+                drawTextPaint(canvas, getItem(position, mOnSectionCallback!!), left, sectionTop, right, sectionBottom)
+                continue
+            }
+
+            if (isLastGroupItem(position, itemCount, mOnSectionCallback!!) && view.bottom <= mSectionHeightPx) {// 分组的最后一个数据并且已经到达顶部，绘制过度动画样式
+                drawBgPaint(canvas, left, top, right, view.bottom)
+                drawTextPaint(canvas, getItem(position, mOnSectionCallback!!), left, top, right, view.bottom)
+                continue
+            }
+
+            if (i == 0) {// 第一个view属于某个分组内的中间数据，则在顶部绘制固定section
+                drawBgPaint(canvas, left, sectionTop, right, sectionBottom)
+                drawTextPaint(canvas, getItem(position, mOnSectionCallback!!), left, sectionTop, right, sectionBottom)
+            }
+        }
+    }
+
+    /** 是否是分组的最后一个数据，位置[position]，列表长度[itemCount] */
+    private fun isLastGroupItem(position: Int, itemCount: Int, callback: OnSectionCallback<T>): Boolean {
+        if (position + 1 >= itemCount) {
+            return true
+        }
+        val current = getItem(position, callback)
+        val next = getItem(position + 1, callback)
+        if (current.isEmpty() || next.isEmpty()) {
+            return false
+        }
+        return !current.substring(0, 1).equals(next.substring(0, 1))
+    }
+}
