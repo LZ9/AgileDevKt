51d66b97391e36227ce395b95408b03b6bd084c9
LZ9 <lz.mms@foxmail.com>
2018-12-26 11:08:49 +0800

    添加九宫格控件

---
+++ componentkt/src/main/java/com/lodz/android/componentkt/widget/ninegrid/OnNineGridViewListener.kt
@@ -0,0 +1,23 @@
+package com.lodz.android.componentkt.widget.ninegrid
+
+import android.content.Context
+import android.widget.ImageView
+
+/**
+ * 九宫格接口
+ * Created by zhouL on 2018/12/25.
+ */
+interface OnNineGridViewListener {
+
+    /** 添加图片，可添加的数量[addCount] */
+    fun onAddPic(addCount: Int)
+
+    /** 展示图片，上下文[context]，数据[data]，控件[imageView] */
+    fun onDisplayImg(context: Context, data: String, imageView: ImageView)
+
+    /** 删除图片，数据[data]，位置[position] */
+    fun onDeletePic(data: String, position: Int)
+
+    /** 点击图片，数据[data]，位置[position] */
+    fun onClickPic(data: String, position: Int)
+}
