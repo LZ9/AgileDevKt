8104bf591743254146d41487b1a3652d562fd607
LZ9 <lz.mms@foxmail.com>
2019-03-22 10:11:04 +0800

    扩展数字转中文方法

--- corekt/src/main/java/com/lodz/android/corekt/anko/AnkoNumFormat.kt
+++ corekt/src/main/java/com/lodz/android/corekt/anko/AnkoNumFormat.kt
@@ -7,7 +7,7 @@ import java.text.DecimalFormat
  * Created by zhouL on 2018/7/18.
  */
 
-object AnkoNumFormat{
+object AnkoNumFormat {
     /** 保留1位小数  */
     const val TYPE_ONE_DECIMAL = "0.0"
     /** 保留2位小数  */
@@ -42,14 +42,39 @@ fun Float.format(formatType: String = AnkoNumFormat.TYPE_TWO_DECIMAL): String {
     return ""
 }
 
-/** 数字转中文 */
+/** 数字转中文（到3位百） */
 fun Int.toChinese(): String {
-    val numCHN = arrayListOf("零", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十",  "百", "千", "万", "亿")
+    val numCHN = arrayListOf("零", "一", "二", "三", "四", "五", "六", "七", "八", "九")
+    val numEmptyCHN = arrayListOf("", "一", "二", "三", "四", "五", "六", "七", "八", "九")
     val numStr = this.toString()
-    if (numStr.length == 1){
+    //个
+    if (numStr.length == 1) {
         return numCHN[this]
     }
-
-
+    //十
+    if (numStr.length == 2) {
+        val ten = numStr.substring(0, 1).toInt()
+        val one = numStr.substring(1, 2).toInt()
+        if (ten == 1) {
+            return "十" + numEmptyCHN[one]
+        }
+        return numEmptyCHN[ten] + "十" + numEmptyCHN[one]
+    }
+    //百
+    if (numStr.length == 3) {
+        if (this == 100) {
+            return "一百"
+        }
+        val hundred = numStr.substring(0, 1).toInt()
+        val ten = numStr.substring(1, 2).toInt()
+        val one = numStr.substring(2, 3).toInt()
+        if (one == 0) {
+            return numEmptyCHN[hundred] + "百" + numCHN[ten] + "十"
+        }
+        if (ten == 0) {
+            return numEmptyCHN[hundred] + "百零" + numEmptyCHN[one]
+        }
+        return numEmptyCHN[hundred] + "百" + numCHN[ten] + "十" + numEmptyCHN[one]
+    }
     return ""
 }
