3f214b72330ebda75ddc5074fa57410399e93642
LZ9 <lz.mms@foxmail.com>
2019-05-24 16:39:39 +0800

    Drawable扩展类增加View扩展

--- corekt/src/main/java/com/lodz/android/corekt/anko/AnkoDrawable.kt
+++ corekt/src/main/java/com/lodz/android/corekt/anko/AnkoDrawable.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
+import android.view.View
 import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
 
@@ -15,8 +16,17 @@ import androidx.annotation.ColorRes
 /** 用颜色资源[color]创建Drawable */
 fun Context.createColorDrawable(@ColorRes color: Int): ColorDrawable = ColorDrawable(getColorCompat(color))
 
+/** 用颜色资源[color]创建Drawable */
+fun View.createColorDrawable(@ColorRes color: Int): ColorDrawable = ColorDrawable(getColorCompat(color))
+
 /** 用颜色[color]创建Drawable */
 fun Context.createColorIntDrawable(@ColorInt color: Int): ColorDrawable = ColorDrawable(color)
 
+/** 用颜色[color]创建Drawable */
+fun View.createColorIntDrawable(@ColorInt color: Int): ColorDrawable = ColorDrawable(color)
+
 /** 用[bitmap]创建Drawable */
 fun Context.createBitmapDrawable(bitmap: Bitmap): BitmapDrawable = BitmapDrawable(resources, bitmap)
+
+/** 用[bitmap]创建Drawable */
+fun View.createBitmapDrawable(bitmap: Bitmap): BitmapDrawable = BitmapDrawable(context.resources, bitmap)
