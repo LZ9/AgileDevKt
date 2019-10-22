740ed0ac5fc0a02bb4053b0cd013dd3ae57f09e4
LZ9 <lz.mms@foxmail.com>
2019-10-22 15:56:32 +0800

    优化底部菜单栏测试类

--- app/src/main/java/com/lodz/android/agiledevkt/modules/menubar/MenuBarTestActivity.kt
+++ app/src/main/java/com/lodz/android/agiledevkt/modules/menubar/MenuBarTestActivity.kt
@@ -7,6 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.tabs.TabLayout
 import com.lodz.android.agiledevkt.R
 import com.lodz.android.agiledevkt.modules.main.MainActivity
@@ -39,14 +42,15 @@ class MenuBarTestActivity : BaseActivity() {
     /** 我的 */
     private val TYPE_MINE = 4
 
-    /** TabLayout */
-    private val mTabLayout by bindView<TabLayout>(R.id.tab_layout)
     /** 提示控件 */
     private val mTabTv by bindView<TextView>(R.id.tab_tv)
-    /** 提示控件 */
-    private val mTipsTv by bindView<TextView>(R.id.tips_tv)
+    /** TabLayout */
+    private val mTabLayout by bindView<TabLayout>(R.id.tab_layout)
     /** 底部菜单栏 */
     private val mMenuBar by bindView<BottomMenuBar>(R.id.menu_bar)
+    /** 底部菜单栏 */
+    private val mBottomNavigationView by bindView<BottomNavigationView>(R.id.bottom_nv)
+
 
     override fun getLayoutId(): Int = R.layout.activity_menu_bar_test
 
@@ -55,29 +59,12 @@ class MenuBarTestActivity : BaseActivity() {
         getTitleBarLayout().setTitleName(intent.getStringExtra(MainActivity.EXTRA_TITLE_NAME) ?: "")
         initMenuBar()
         initTabLayout()
-    }
-
-    private fun initTabLayout() {
-        val titles = arrayListOf("首页", "功能", "消息", "我的")
-        titles.forEachIndexed { i, title ->
-            val tabView = LayoutInflater.from(getContext()).inflate(R.layout.view_tab, null)
-            val iconImg = tabView.findViewById<ImageView>(com.lodz.android.pandora.R.id.ic_img)
-            val textTv = tabView.findViewById<TextView>(com.lodz.android.pandora.R.id.text_tv)
-            val numTv = tabView.findViewById<TextView>(com.lodz.android.pandora.R.id.num_tv)
-
-            iconImg.setImageResource(R.drawable.selector_ic_tab)
-            textTv.text = title
-            numTv.text = (i + 1).toString()
-            numTv.visibility = View.VISIBLE
-            mTabLayout.addTab(mTabLayout.newTab().setCustomView(tabView), i == 0)
-        }
-        mTabTv.text = "首页"
+        initNavigation()
     }
 
     private fun initMenuBar() {
         mMenuBar.setMenuConfigs(getConfigs())
         mMenuBar.setSelectedMenu(TYPE_HOME)
-        mTipsTv.text = "首页"
     }
 
     private fun getConfigs(): List<MenuConfig> {
@@ -93,9 +80,9 @@ class MenuBarTestActivity : BaseActivity() {
         val config = MenuConfig()
         config.setType(type)
         config.setIconSize(30)
-        config.setIconResId(getContext(), R.drawable.pandora_ic_take_photo_cancel, R.drawable.pandora_ic_take_photo_confirm)
+        config.setIconResId(getContext(), R.drawable.ic_person, R.drawable.ic_person_sel)
         config.setText(text)
-        config.setTextColor(getContext(), R.color.white, R.color.color_b3e5fc)
+        config.setTextColor(getContext(), R.color.color_666666, R.color.color_00a0e9)
 //        config.setTextSize(18f)
         config.setNum(num)
 //        config.setNumTextColor(getContext(), R.color.color_00a0e9)
@@ -103,6 +90,39 @@ class MenuBarTestActivity : BaseActivity() {
 //        config.setNumBackground(R.drawable.bg_ffffff_circle)
         config.setDrawablePadding(dp2px(2))
         return config
+    }
+
+    private fun initTabLayout() {
+        val titles = arrayListOf(
+            getString(R.string.menu_bar_home), getString(R.string.menu_bar_module),
+            getString(R.string.menu_bar_msg), getString(R.string.menu_bar_mine)
+        )
+        titles.forEachIndexed { i, title ->
+            val tabView = LayoutInflater.from(getContext()).inflate(R.layout.view_tab, null)
+            val iconImg = tabView.findViewById<ImageView>(com.lodz.android.pandora.R.id.ic_img)
+            val textTv = tabView.findViewById<TextView>(com.lodz.android.pandora.R.id.text_tv)
+            val numTv = tabView.findViewById<TextView>(com.lodz.android.pandora.R.id.num_tv)
+
+            iconImg.setImageResource(R.drawable.selector_ic_tab)
+            textTv.text = title
+            numTv.text = (i + 1).toString()
+            numTv.visibility = if (i % 2 == 1) View.VISIBLE else View.GONE
+            mTabLayout.addTab(mTabLayout.newTab().setCustomView(tabView), i == 0)
+        }
+        mTabTv.text = "首页"
+    }
+
+    private fun initNavigation() {
+        mBottomNavigationView.itemIconTintList = null
+        val menuView = mBottomNavigationView.getChildAt(0) as BottomNavigationMenuView
+        for (i in 0 until menuView.childCount) {
+            val itemView = menuView.getChildAt(i) as BottomNavigationItemView
+            val badge = LayoutInflater.from(getContext()).inflate(R.layout.view_badge, itemView, false)
+            itemView.addView(badge)
+            val numTv = badge.findViewById<TextView>(R.id.num_tv)
+            numTv.text = (i + 8).toString()
+            numTv.visibility = View.VISIBLE
+        }
     }
 
     override fun onClickBackBtn() {
@@ -125,22 +145,33 @@ class MenuBarTestActivity : BaseActivity() {
                 if (textTv != null){
                     mTabTv.text = textTv.text
                 }
-                val numTv = tab?.customView?.findViewById<TextView>(R.id.num_tv)
-                if (numTv != null){
-                    numTv.visibility = View.GONE
-                }
+//                val numTv = tab?.customView?.findViewById<TextView>(R.id.num_tv)
+//                if (numTv != null){
+//                    numTv.visibility = View.GONE
+//                }
             }
         })
 
         mMenuBar.setOnSelectedListener { type ->
-            mTipsTv.text =when(type){
+            mTabTv.text =when(type){
                 TYPE_HOME -> "首页"
                 TYPE_WORK -> "功能"
                 TYPE_MSG -> "消息"
                 TYPE_MINE -> "我的"
                 else -> ""
             }
-            mMenuBar.updateNum(type, 0)
+//            mMenuBar.updateNum(type, 0)`
+        }
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            mTabTv.text =when(item.itemId){
+                R.id.develop_tab -> "首页"
+                R.id.product_tab -> "功能"
+                R.id.project_tab -> "消息"
+                R.id.personnel_tab -> "我的"
+                else -> ""
+            }
+            return@setOnNavigationItemSelectedListener true
         }
     }
 
