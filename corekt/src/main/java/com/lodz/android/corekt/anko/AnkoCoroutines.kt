eb882442b58323efda9a998477112052abc0ebe9
LZ9 <lz.mms@foxmail.com>
2019-11-18 11:25:48 +0800

    AnkoCoroutines去掉主线程挂起函数

--- corekt/src/main/java/com/lodz/android/corekt/anko/AnkoCoroutines.kt
+++ corekt/src/main/java/com/lodz/android/corekt/anko/AnkoCoroutines.kt
@@ -11,16 +11,14 @@ import kotlinx.coroutines.*
 /** 主线程执行 */
 fun runOnMain(block: () -> Unit): Job = GlobalScope.launch(Dispatchers.Main) { block() }
 
-/** 主线程执行 */
-fun runOnSuspendMain(block: suspend () -> Unit): Job = GlobalScope.launch(Dispatchers.Main) { block() }
-
 /** 主线程延迟[timeMillis]毫秒执行 */
-fun runOnMainDelay(timeMillis: Long, block: () -> Unit): Job = GlobalScope.launch(Dispatchers.IO) {
-    delay(timeMillis)
-    GlobalScope.launch(Dispatchers.Main) {
-        block()
+fun runOnMainDelay(timeMillis: Long, block: () -> Unit): Job =
+    GlobalScope.launch(Dispatchers.IO) {
+        delay(timeMillis)
+        GlobalScope.launch(Dispatchers.Main) {
+            block()
+        }
     }
-}
 
 /** 异步线程执行 */
 fun runOnIO(block: () -> Unit): Job = GlobalScope.launch(Dispatchers.IO) { block() }
