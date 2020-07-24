d472b184d77ae20428deaddac83d911ef3d4fac1
LZ9 <lz.mms@foxmail.com>
2020-07-24 18:00:15 +0800

    修改mock地址

--- app/src/main/java/com/lodz/android/agiledevkt/config/UrlConfig.kt
+++ app/src/main/java/com/lodz/android/agiledevkt/config/UrlConfig.kt
@@ -12,8 +12,7 @@ object UrlConfig {
     @Keep
     private const val Release = "http://www.baidu.com/" // 正式地址
     @Keep
-    private const val Test = "http://192.168.5.210:8080/app/mock/32/POST/yszz/" // 测试地址
-
+    private const val Test = "http://192.168.5.204:38080/app/mock/32/yszz/" // 测试地址
 
     /** 正式环境 */
     const val BASE_URL = Test
