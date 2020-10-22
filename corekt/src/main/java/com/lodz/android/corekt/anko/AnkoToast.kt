23be92ced0b990ee0768dc97b888fd451ad6cc96
LZ9 <lz.mms@foxmail.com>
2020-10-22 18:06:12 +0800

    AnkoToast增加扩展方法

--- corekt/src/main/java/com/lodz/android/corekt/anko/AnkoToast.kt
+++ corekt/src/main/java/com/lodz/android/corekt/anko/AnkoToast.kt
@@ -1,6 +1,8 @@
 package com.lodz.android.corekt.anko
 
+import android.app.Dialog
 import android.content.Context
+import android.view.View
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import com.lodz.android.corekt.utils.ToastUtils
@@ -48,4 +50,44 @@ fun Fragment.toastLong(text: String) {
 /** 显示文字资源为[strResId]的长时间的Toast */
 fun Fragment.toastLong(@StringRes strResId: Int) {
     context?.toastLong(strResId)
+}
+
+/** 显示文字为[text]的短时间的Toast */
+fun View.toastShort(text: String) {
+    context.toastShort(text)
+}
+
+/** 显示文字资源为[strResId]的短时间的Toast */
+fun View.toastShort(@StringRes strResId: Int) {
+    context.toastShort(strResId)
+}
+
+/** 显示文字为[text]的长时间的Toast */
+fun View.toastLong(text: String) {
+    context.toastLong(text)
+}
+
+/** 显示文字资源为[strResId]的长时间的Toast */
+fun View.toastLong(@StringRes strResId: Int) {
+    context.toastLong(strResId)
+}
+
+/** 显示文字为[text]的短时间的Toast */
+fun Dialog.toastShort(text: String) {
+    context.toastShort(text)
+}
+
+/** 显示文字资源为[strResId]的短时间的Toast */
+fun Dialog.toastShort(@StringRes strResId: Int) {
+    context.toastShort(strResId)
+}
+
+/** 显示文字为[text]的长时间的Toast */
+fun Dialog.toastLong(text: String) {
+    context.toastLong(text)
+}
+
+/** 显示文字资源为[strResId]的长时间的Toast */
+fun Dialog.toastLong(@StringRes strResId: Int) {
+    context.toastLong(strResId)
 }
