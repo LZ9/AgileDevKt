d420157ae84c247e00897cc54c27e1eb89a11b78
zhouL <lz.mms@foxmail.com>
2021-08-10 14:06:07 +0800

    RxObservableActivity.kt支持ViewBinding

--- app/src/main/java/com/lodz/android/agiledevkt/modules/rxjava/observable/RxObservableActivity.kt
+++ app/src/main/java/com/lodz/android/agiledevkt/modules/rxjava/observable/RxObservableActivity.kt
@@ -3,15 +3,11 @@ package com.lodz.android.agiledevkt.modules.rxjava.observable
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.ScrollView
-import android.widget.TextView
-import androidx.core.widget.NestedScrollView
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.switchmaterial.SwitchMaterial
-import com.lodz.android.agiledevkt.R
 import com.lodz.android.agiledevkt.bean.base.response.ResponseBean
+import com.lodz.android.agiledevkt.databinding.ActivityRxObservableBinding
 import com.lodz.android.agiledevkt.modules.main.MainActivity
-import com.lodz.android.corekt.anko.bindView
 import com.lodz.android.corekt.anko.runOnMainDelay
 import com.lodz.android.corekt.anko.then
 import com.lodz.android.pandora.base.activity.BaseActivity
@@ -22,6 +18,7 @@ import com.lodz.android.pandora.rx.utils.RxUtils
 import com.lodz.android.pandora.rx.utils.doComplete
 import com.lodz.android.pandora.rx.utils.doError
 import com.lodz.android.pandora.rx.utils.doNext
+import com.lodz.android.pandora.utils.viewbinding.bindingLayout
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
@@ -41,26 +38,9 @@ class RxObservableActivity : BaseActivity() {
         }
     }
 
-    /** 滚动控件 */
-    private val mScrollView by bindView<NestedScrollView>(R.id.scroll_view)
-    /** 结果 */
-    private val mResultTv by bindView<TextView>(R.id.result_tv)
-    /** 订阅失败开关 */
-    private val mFailSwitch by bindView<SwitchMaterial>(R.id.fail_switch)
-    /** 返回键关闭开关 */
-    private val mCancelableSwitch by bindView<SwitchMaterial>(R.id.cancelable_switch)
-    /** 空白处关闭开关 */
-    private val mCanceledOutsideSwitch by bindView<SwitchMaterial>(R.id.canceled_outside_switch)
-    /** 原始订阅接口 */
-    private val mOriginalBtn by bindView<MaterialButton>(R.id.original_btn)
-    /** 基础封装 */
-    private val mBaseBtn by bindView<MaterialButton>(R.id.base_btn)
-    /** 响应数据封装 */
-    private val mRxBtn by bindView<MaterialButton>(R.id.rx_btn)
-    /** 进度条封装 */
-    private val mProgressBtn by bindView<MaterialButton>(R.id.progress_btn)
+    private val mBinding: ActivityRxObservableBinding by bindingLayout(ActivityRxObservableBinding::inflate)
 
-    override fun getLayoutId(): Int = R.layout.activity_rx_observable
+    override fun getViewBindingLayout(): View = mBinding.root
 
     override fun findViews(savedInstanceState: Bundle?) {
         super.findViews(savedInstanceState)
@@ -76,15 +56,15 @@ class RxObservableActivity : BaseActivity() {
         super.setListeners()
 
         // 返回键关闭开关
-        mCancelableSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+        mBinding.cancelableSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
             if (!isChecked) {
-                mCanceledOutsideSwitch.isChecked = false
+                mBinding.canceledOutsideSwitch.isChecked = false
             }
-            mCanceledOutsideSwitch.isEnabled = isChecked
+            mBinding.canceledOutsideSwitch.isEnabled = isChecked
         }
 
         // 原始订阅接口
-        mOriginalBtn.setOnClickListener {
+        mBinding.originalBtn.setOnClickListener {
             cleanLog()
             createObservable()
                     .compose(RxUtils.ioToMainObservable())
@@ -109,40 +89,47 @@ class RxObservableActivity : BaseActivity() {
         }
 
         // 基础封装
-        mBaseBtn.setOnClickListener {
+        mBinding.baseBtn.setOnClickListener {
             cleanLog()
             createObservable()
                     .compose(RxUtils.ioToMainObservable())
                     .compose(bindDestroyEvent())
-                    .subscribe(BaseObserver.action({ any ->
-                        printLog("onBaseNext num : $any")
+                    .subscribe(BaseObserver.action({
+                        printLog("onBaseNext num : $it")
                     }, { e ->
                         printLog("onBaseError message : ${e.message}")
                     }))
         }
 
         // 响应数据封装
-        mRxBtn.setOnClickListener {
+        mBinding.rxBtn.setOnClickListener {
             cleanLog()
             createObservable(false)
                     .compose(RxUtils.ioToMainObservable())
                     .compose(bindDestroyEvent())
-                    .subscribe(RxObserver.action({ any ->
-                        printLog("onRxNext : ${any.data}")
+                    .subscribe(RxObserver.action({
+                        printLog("onRxNext : ${it.data}")
                     }, { e, isNetwork ->
                         printLog("onRxError message : ${RxUtils.getExceptionTips(e, isNetwork, "create fail")}")
                     }))
         }
 
         // 进度条封装
-        mProgressBtn.setOnClickListener {
+        mBinding.progressBtn.setOnClickListener {
             cleanLog()
             createObservable(true)
                 .compose(RxUtils.ioToMainObservable())
                 .compose(bindDestroyEvent())
-                .subscribe(ProgressObserver.action(getContext(),"loading", mCancelableSwitch.isChecked, mCanceledOutsideSwitch.isChecked,
-                    { any -> printLog("onPgNext num : ${any.data}") },
-                    { e, isNetwork -> printLog("onPgError message : ${RxUtils.getExceptionTips(e, isNetwork,"create fail")}")}))
+                .subscribe(
+                    ProgressObserver.action(getContext(),
+                        msg ="loading",
+                        cancelable = mBinding.cancelableSwitch.isChecked,
+                        canceledOnTouchOutside = mBinding.canceledOutsideSwitch.isChecked,
+                        next = { printLog("onPgNext num : ${it.data}") },
+                        error = { e, isNetwork ->
+                            printLog("onPgError message : ${RxUtils.getExceptionTips(e, isNetwork,"create fail")}")
+                        })
+                )
         }
     }
 
@@ -160,7 +147,7 @@ class RxObservableActivity : BaseActivity() {
             if (emitter.isDisposed) {
                 return@create
             }
-            if (mFailSwitch.isChecked) {
+            if (mBinding.failSwitch.isChecked) {
                 emitter.doError(RuntimeException("create fail"))
                 return@create
             }
@@ -180,7 +167,7 @@ class RxObservableActivity : BaseActivity() {
     private fun createObservable(isDelay: Boolean): Observable<ResponseBean<String>> =
         Observable.create { emitter ->
             val delayTime = isDelay.then { 3 } ?: 0
-            val responseBean: ResponseBean<String> = if (mFailSwitch.isChecked) {
+            val responseBean: ResponseBean<String> = if (mBinding.failSwitch.isChecked) {
                 val bean: ResponseBean<String> = ResponseBean.createFail()
                 bean.msg = "数据获取失败"
                 bean
@@ -201,19 +188,19 @@ class RxObservableActivity : BaseActivity() {
 
     /** 清空日志 */
     private fun cleanLog() {
-        mResultTv.text = ""
+        mBinding.resultTv.text = ""
     }
 
     /** 打印日志 */
     private fun printLog(text: String) {
-        val log = if (mResultTv.text.isEmpty()) {
+        val log = if (mBinding.resultTv.text.isEmpty()) {
             text
         } else {
-            "${mResultTv.text}\n$text"
+            "${mBinding.resultTv.text}\n$text"
         }
-        mResultTv.text = log
+        mBinding.resultTv.text = log
         GlobalScope.runOnMainDelay(100) {
-            mScrollView.fullScroll(ScrollView.FOCUS_DOWN)
+            mBinding.scrollView.fullScroll(ScrollView.FOCUS_DOWN)
         }
     }
 }
