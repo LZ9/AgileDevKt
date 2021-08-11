cbf212cd0076e4861ebcdee697e261c56191389a
zhouL <lz.mms@foxmail.com>
2021-08-11 10:40:42 +0800

    SnackbarTestActivity.kt支持ViewBinding

--- app/src/main/java/com/lodz/android/agiledevkt/modules/snackbar/SnackbarTestActivity.kt
+++ app/src/main/java/com/lodz/android/agiledevkt/modules/snackbar/SnackbarTestActivity.kt
@@ -4,19 +4,18 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
-import androidx.coordinatorlayout.widget.CoordinatorLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.lodz.android.agiledevkt.R
+import com.lodz.android.agiledevkt.databinding.ActivitySnackbarTestBinding
 import com.lodz.android.agiledevkt.modules.main.MainActivity
-import com.lodz.android.corekt.anko.bindView
 import com.lodz.android.corekt.anko.dp2px
 import com.lodz.android.corekt.anko.getColorCompat
 import com.lodz.android.corekt.anko.toastShort
 import com.lodz.android.corekt.utils.SnackbarUtils
 import com.lodz.android.pandora.base.activity.AbsActivity
-import com.lodz.android.pandora.widget.base.TitleBarLayout
+import com.lodz.android.pandora.utils.viewbinding.bindingLayout
 
 /**
  * Snackbar测试类
@@ -31,34 +30,24 @@ class SnackbarTestActivity : AbsActivity() {
         }
     }
 
-    /** 根布局 */
-    private val mRootView by bindView<CoordinatorLayout>(R.id.root_view)
-    /** 标题栏 */
-    private val mTitleBarLayout by bindView<TitleBarLayout>(R.id.title_bar_layout)
-    /** 显示短的snackbar按钮 */
-    private val mShortBtn by bindView<FloatingActionButton>(R.id.short_btn)
-    /** 关注按钮 */
-    private val mAttentionBtn by bindView<Button>(R.id.attention_btn)
-    /** 自定义图片按钮 */
-    private val mCustomImgBtn by bindView<Button>(R.id.custom_img_btn)
-    /** 自定义布局按钮 */
-    private val mCustomLayoutBtn by bindView<Button>(R.id.custom_layout_btn)
+    private val mBinding: ActivitySnackbarTestBinding by bindingLayout(ActivitySnackbarTestBinding::inflate)
 
-    override fun getAbsLayoutId() = R.layout.activity_snackbar_test
+    override fun getAbsViewBindingLayout(): View = mBinding.root
 
     override fun findViews(savedInstanceState: Bundle?) {
-        mTitleBarLayout.setTitleName(intent.getStringExtra(MainActivity.EXTRA_TITLE_NAME) ?: "")
+        mBinding.titleBarLayout.setTitleName(intent.getStringExtra(MainActivity.EXTRA_TITLE_NAME) ?: "")
     }
 
     override fun setListeners() {
         super.setListeners()
 
-        mTitleBarLayout.setOnBackBtnClickListener {
+        mBinding.titleBarLayout.setOnBackBtnClickListener {
             finish()
         }
 
-        mShortBtn.setOnClickListener {
-            SnackbarUtils.createShort(mRootView, R.string.snackbar_sign_title)
+        // 显示短的snackbar按钮
+        mBinding.shortBtn.setOnClickListener {
+            SnackbarUtils.createShort(mBinding.root, R.string.snackbar_sign_title)
                     .setTextColor(Color.WHITE)
                     .setTextSize(17)
                     .setBackgroundColor(getColorCompat(R.color.color_00a0e9))
@@ -73,8 +62,8 @@ class SnackbarTestActivity : AbsActivity() {
         }
 
         // 关注按钮
-        mAttentionBtn.setOnClickListener {
-            SnackbarUtils.createLong(mRootView, R.string.snackbar_attention_title)
+        mBinding.attentionBtn.setOnClickListener {
+            SnackbarUtils.createLong(mBinding.root, R.string.snackbar_attention_title)
                     .setTextColor(Color.WHITE)
                     .setTextSize(15)
                     .setBackgroundColor(getColorCompat(R.color.color_ea5e5e))
@@ -88,20 +77,20 @@ class SnackbarTestActivity : AbsActivity() {
         }
 
         // 自定义图片按钮
-        mCustomImgBtn.setOnClickListener {
+        mBinding.customImgBtn.setOnClickListener {
             val imageView = ImageView(getContext())
             imageView.setImageResource(R.drawable.ic_regret)
             imageView.scaleType = ImageView.ScaleType.CENTER_CROP
 
-            SnackbarUtils.createLong(mRootView, R.string.snackbar_custom_img)
+            SnackbarUtils.createLong(mBinding.root, R.string.snackbar_custom_img)
                     .setBackgroundColor(Color.WHITE)
                     .replaceLayoutView(imageView)
                     .show()
         }
 
         // 自定义布局按钮
-        mCustomLayoutBtn.setOnClickListener {
-            val snackbar = SnackbarUtils.createLong(mRootView, R.string.snackbar_custom_layout)
+        mBinding.customLayoutBtn.setOnClickListener {
+            val snackbar = SnackbarUtils.createLong(mBinding.root, R.string.snackbar_custom_layout)
                     .setBackgroundColor(Color.WHITE)
                     .replaceLayoutView(R.layout.snackbar_custom)
                     .getSnackbar()
