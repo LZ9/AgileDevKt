e538f5cd6afeb0799e8a7f8ff68db833cd5a1e07
zhouL <lz.mms@foxmail.com>
2021-08-12 10:14:28 +0800

    ThreadPoolActivity.kt支持ViewBinding

--- app/src/main/java/com/lodz/android/agiledevkt/modules/threadpool/ThreadPoolActivity.kt
+++ app/src/main/java/com/lodz/android/agiledevkt/modules/threadpool/ThreadPoolActivity.kt
@@ -3,16 +3,15 @@ package com.lodz.android.agiledevkt.modules.threadpool
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
 import android.widget.ScrollView
-import android.widget.TextView
-import com.lodz.android.agiledevkt.R
+import com.lodz.android.agiledevkt.databinding.ActivityThreadPoolBinding
 import com.lodz.android.agiledevkt.modules.main.MainActivity
-import com.lodz.android.corekt.anko.bindView
 import com.lodz.android.corekt.anko.runOnMain
 import com.lodz.android.corekt.anko.runOnMainDelay
 import com.lodz.android.corekt.threadpool.ThreadPoolManager
 import com.lodz.android.pandora.base.activity.BaseActivity
+import com.lodz.android.pandora.utils.viewbinding.bindingLayout
 import kotlinx.coroutines.GlobalScope
 import java.util.concurrent.ThreadPoolExecutor
 import java.util.concurrent.TimeUnit
@@ -30,19 +29,9 @@ class ThreadPoolActivity : BaseActivity() {
         }
     }
 
-    /** 开始按钮 */
-    private val mStartBtn by bindView<Button>(R.id.start_btn)
-    /** 关闭按钮 */
-    private val mCloseBtn by bindView<Button>(R.id.close_btn)
-    /** 清空按钮 */
-    private val mClearBtn by bindView<Button>(R.id.clear_btn)
+    private val mBinding: ActivityThreadPoolBinding by bindingLayout(ActivityThreadPoolBinding::inflate)
 
-    /** 滚动控件 */
-    private val mScrollView by bindView<ScrollView>(R.id.scroll_view)
-    /** 结果 */
-    private val mResultTv by bindView<TextView>(R.id.result_tv)
-
-    override fun getLayoutId() = R.layout.activity_thread_pool
+    override fun getViewBindingLayout(): View = mBinding.root
 
     override fun findViews(savedInstanceState: Bundle?) {
         getTitleBarLayout().setTitleName(intent.getStringExtra(MainActivity.EXTRA_TITLE_NAME) ?: "")
@@ -57,7 +46,7 @@ class ThreadPoolActivity : BaseActivity() {
         super.setListeners()
 
         // 开始按钮
-        mStartBtn.setOnClickListener {
+        mBinding.startBtn.setOnClickListener {
             startLongThread()
             for (i in 0 until 30) {
                 startShortThread(i)
@@ -65,12 +54,13 @@ class ThreadPoolActivity : BaseActivity() {
         }
 
         // 关闭按钮
-        mCloseBtn.setOnClickListener {
+        mBinding.closeBtn.setOnClickListener {
             ThreadPoolManager.get().releaseNormalExecutor()
         }
 
-        mClearBtn.setOnClickListener {
-            mResultTv.text = ""
+        // 清空按钮
+        mBinding.clearBtn.setOnClickListener {
+            mBinding.resultTv.text = ""
         }
     }
 
@@ -111,24 +101,24 @@ class ThreadPoolActivity : BaseActivity() {
     override fun initData() {
         super.initData()
         ThreadPoolManager.get().newBuilder()
-                .setAwaitTime(50)// 设置线程结束等待时间
-                .setAwaitTimeUnit(TimeUnit.MILLISECONDS)// 设置线程结束等待时间单位
-                .setKeepAliveTime(1)// 设置线程数空闲时间
-                .setKeepAliveTimeUnit(TimeUnit.SECONDS)// 设置线程数空闲时间单位
-                .setCorePoolSize(4)// 设置线程数
-                .setMaximumPoolSize(8)// 设置最大线程数
-                .setRejectedExecutionHandler(ThreadPoolExecutor.DiscardPolicy())// 设置拒绝策略
-                .build()
+            .setAwaitTime(50)// 设置线程结束等待时间
+            .setAwaitTimeUnit(TimeUnit.MILLISECONDS)// 设置线程结束等待时间单位
+            .setKeepAliveTime(1)// 设置线程数空闲时间
+            .setKeepAliveTimeUnit(TimeUnit.SECONDS)// 设置线程数空闲时间单位
+            .setCorePoolSize(4)// 设置线程数
+            .setMaximumPoolSize(8)// 设置最大线程数
+            .setRejectedExecutionHandler(ThreadPoolExecutor.DiscardPolicy())// 设置拒绝策略
+            .build()
         showStatusCompleted()
     }
 
     /** 打印信息[result] */
     private fun printResult(result: String) {
         GlobalScope.runOnMain {
-            mResultTv.text = (mResultTv.text.toString() + "\n" + result)
+            mBinding.resultTv.text = (mBinding.resultTv.text.toString() + "\n" + result)
         }
         GlobalScope.runOnMainDelay(100) {
-            mScrollView.fullScroll(ScrollView.FOCUS_DOWN)
+            mBinding.scrollView.fullScroll(ScrollView.FOCUS_DOWN)
         }
     }
 
