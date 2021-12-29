ade28d2dfe93fdb34d2207cb2620dd6f96b81031
zhouL <lz.mms@foxmail.com>
2021-12-29 14:08:31 +0800

    增加ping方法

--- corekt/src/main/java/com/lodz/android/corekt/anko/AnkoString.kt
+++ corekt/src/main/java/com/lodz/android/corekt/anko/AnkoString.kt
@@ -1,6 +1,7 @@
 package com.lodz.android.corekt.anko
 
 import android.text.Editable
+import java.io.IOException
 
 /**
  * String扩展类
@@ -14,3 +15,13 @@ fun CharSequence.toEditable(): Editable = Editable.Factory.getInstance().newEdit
 /** 在String尾部进行文字拼接 */
 fun <T> String.append(t: T?): String = StringBuilder(this).append((t != null).then { t } ?: "").toString()
 
+
+/**
+ * ping一个ip地址，true表示连通，false表示不连通
+ */
+@Throws(InterruptedException::class, IOException::class)
+fun String.pingIp(): Boolean {
+    val p = Runtime.getRuntime().exec("ping -c 1 -w 2 -s 32 $this")
+    val status = p.waitFor()
+    return status == 0 // 0表示ping通，非0表示ping不通
+}
