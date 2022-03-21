a7f8c819d4015bda785e88413d9f257429d3d01c
zhouL <lz.mms@foxmail.com>
2022-03-21 19:16:59 +0800

    添加创建圆角Drawable的方法

--- corekt/src/main/java/com/lodz/android/corekt/anko/AnkoDrawable.kt
+++ corekt/src/main/java/com/lodz/android/corekt/anko/AnkoDrawable.kt
@@ -2,11 +2,16 @@ package com.lodz.android.corekt.anko
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.Paint
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.view.View
 import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.RoundedCornerTreatment
+import com.google.android.material.shape.ShapeAppearanceModel
 
 /**
  * Drawable扩展类
@@ -17,16 +22,59 @@ import androidx.annotation.ColorRes
 fun Context.createColorDrawable(@ColorRes color: Int): ColorDrawable = ColorDrawable(getColorCompat(color))
 
 /** 用颜色资源[color]创建Drawable */
-fun View.createColorDrawable(@ColorRes color: Int): ColorDrawable = ColorDrawable(getColorCompat(color))
+fun View.createColorDrawable(@ColorRes color: Int): ColorDrawable = context.createColorDrawable(color)
+
+/** 用颜色资源[color]创建Drawable */
+fun RecyclerView.ViewHolder.createColorDrawable(@ColorRes color: Int): ColorDrawable = itemView.createColorDrawable(color)
 
 /** 用颜色[color]创建Drawable */
 fun Context.createColorIntDrawable(@ColorInt color: Int): ColorDrawable = ColorDrawable(color)
 
 /** 用颜色[color]创建Drawable */
-fun View.createColorIntDrawable(@ColorInt color: Int): ColorDrawable = ColorDrawable(color)
+fun View.createColorIntDrawable(@ColorInt color: Int): ColorDrawable = context.createColorIntDrawable(color)
+
+/** 用颜色[color]创建Drawable */
+fun RecyclerView.ViewHolder.createColorIntDrawable(@ColorInt color: Int): ColorDrawable = itemView.createColorIntDrawable(color)
 
 /** 用[bitmap]创建Drawable */
 fun Context.createBitmapDrawable(bitmap: Bitmap): BitmapDrawable = BitmapDrawable(resources, bitmap)
 
 /** 用[bitmap]创建Drawable */
-fun View.createBitmapDrawable(bitmap: Bitmap): BitmapDrawable = BitmapDrawable(context.resources, bitmap)
+fun View.createBitmapDrawable(bitmap: Bitmap): BitmapDrawable = context.createBitmapDrawable(bitmap)
+
+/** 用[bitmap]创建Drawable */
+fun RecyclerView.ViewHolder.createBitmapDrawable(bitmap: Bitmap): BitmapDrawable = itemView.createBitmapDrawable(bitmap)
+
+/** 用颜色资源[color]创建圆角为[allCornerSizePx]的Drawable */
+fun Context.createCornerDrawable(@ColorRes color: Int, allCornerSizePx: Float): MaterialShapeDrawable =
+    createCornerDrawable(color, getDefaultShapeModel(allCornerSizePx))
+
+/** 用颜色资源[color]创建圆角为[allCornerSizePx]的Drawable */
+fun View.createCornerDrawable(@ColorRes color: Int, allCornerSizePx: Float): MaterialShapeDrawable =
+    context.createCornerDrawable(color, allCornerSizePx)
+
+/** 用颜色资源[color]创建圆角为[allCornerSizePx]的Drawable */
+fun RecyclerView.ViewHolder.createCornerDrawable(@ColorRes color: Int, allCornerSizePx: Float): MaterialShapeDrawable =
+    itemView.createCornerDrawable(color, allCornerSizePx)
+
+/** 用颜色资源[color]创建圆角模式为[shapeModel]的Drawable */
+fun Context.createCornerDrawable(@ColorRes color: Int, shapeModel: ShapeAppearanceModel): MaterialShapeDrawable =
+    MaterialShapeDrawable(shapeModel).apply {
+        setTint(getColorCompat(color))
+        paintStyle = Paint.Style.FILL
+    }
+
+/** 用颜色资源[color]创建圆角模式为[shapeModel]的Drawable */
+fun View.createCornerDrawable(@ColorRes color: Int, shapeModel: ShapeAppearanceModel): MaterialShapeDrawable =
+    context.createCornerDrawable(color, shapeModel)
+
+/** 用颜色资源[color]创建圆角模式为[shapeModel]的Drawable */
+fun RecyclerView.ViewHolder.createCornerDrawable(@ColorRes color: Int, shapeModel: ShapeAppearanceModel): MaterialShapeDrawable =
+    itemView.createCornerDrawable(color, shapeModel)
+
+/** 获取默认的样式配置模型，圆角大小[allCornerSizePx] */
+private fun getDefaultShapeModel(allCornerSizePx: Float): ShapeAppearanceModel =
+    ShapeAppearanceModel.builder()
+        .setAllCorners(RoundedCornerTreatment())
+        .setAllCornerSizes(allCornerSizePx)
+        .build()
