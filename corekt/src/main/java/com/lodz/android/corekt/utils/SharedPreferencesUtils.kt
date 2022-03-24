5e2184e59ac2a9babc147946c358256c09bcb6c6
zhouL <lz.mms@foxmail.com>
2022-03-24 14:51:01 +0800

    SP内部的上下文使用弱关联

--- corekt/src/main/java/com/lodz/android/corekt/utils/SharedPreferencesUtils.kt
+++ corekt/src/main/java/com/lodz/android/corekt/utils/SharedPreferencesUtils.kt
@@ -2,6 +2,7 @@ package com.lodz.android.corekt.utils
 
 import android.content.Context
 import androidx.core.content.edit
+import java.lang.ref.WeakReference
 
 /**
  * SP工具类
@@ -18,7 +19,7 @@ class SharedPreferencesUtils private constructor() {
         fun get(): SharedPreferencesUtils = sInstance
     }
 
-    private var mContext: Context? = null
+    private var mContext: WeakReference<Context>? = null
 
     private var mName: String = SP_NAME
 
@@ -27,101 +28,101 @@ class SharedPreferencesUtils private constructor() {
     /** 初始化[context]上下文，[name]文件名，[mode]模式 */
     @JvmOverloads
     fun init(context: Context, name: String = SP_NAME, mode: Int = Context.MODE_PRIVATE) {
-        mContext = context
+        mContext = WeakReference(context)
         mName = name
         mMode = mode
     }
 
     /** 保存String类型数据,键[key]，值[value] */
     fun putString(key: String, value: String) {
-        mContext?.getSharedPreferences(mName, mMode)?.edit {
+        mContext?.get()?.getSharedPreferences(mName, mMode)?.edit {
             this.putString(key, value)
         }
     }
 
     /** 获取String型数据,键[key]，默认值[defValue] */
     fun getString(key: String, defValue: String): String = catch(defValue) {
-        mContext?.getSharedPreferences(mName, mMode)?.getString(key, defValue) ?: defValue
+        mContext?.get()?.getSharedPreferences(mName, mMode)?.getString(key, defValue) ?: defValue
     }
 
     /** 保存Boolean类型数据,键[key]，值[value] */
     fun putBoolean(key: String, value: Boolean) {
-        mContext?.getSharedPreferences(mName, mMode)?.edit {
+        mContext?.get()?.getSharedPreferences(mName, mMode)?.edit {
             this.putBoolean(key, value)
         }
     }
 
     /** 获取Boolean型数据,键[key]，默认值[defValue] */
     fun getBoolean(key: String, defValue: Boolean): Boolean = catch(defValue) {
-        mContext?.getSharedPreferences(mName, mMode)?.getBoolean(key, defValue) ?: defValue
+        mContext?.get()?.getSharedPreferences(mName, mMode)?.getBoolean(key, defValue) ?: defValue
     }
 
     /** 保存Float类型数据,键[key]，值[value] */
     fun putFloat(key: String, value: Float) {
-        mContext?.getSharedPreferences(mName, mMode)?.edit {
+        mContext?.get()?.getSharedPreferences(mName, mMode)?.edit {
             this.putFloat(key, value)
         }
     }
 
     /** 获取Float型数据,键[key]，默认值[defValue] */
     fun getFloat(key: String, defValue: Float): Float = catch(defValue) {
-        mContext?.getSharedPreferences(mName, mMode)?.getFloat(key, defValue) ?: defValue
+        mContext?.get()?.getSharedPreferences(mName, mMode)?.getFloat(key, defValue) ?: defValue
     }
 
     /** 保存Int类型数据,键[key]，值[value] */
     fun putInt(key: String, value: Int) {
-        mContext?.getSharedPreferences(mName, mMode)?.edit {
+        mContext?.get()?.getSharedPreferences(mName, mMode)?.edit {
             this.putInt(key, value)
         }
     }
 
     /** 获取Int型数据,键[key]，默认值[defValue] */
     fun getInt(key: String, defValue: Int): Int = catch(defValue) {
-        mContext?.getSharedPreferences(mName, mMode)?.getInt(key, defValue) ?: defValue
+        mContext?.get()?.getSharedPreferences(mName, mMode)?.getInt(key, defValue) ?: defValue
     }
 
     /** 保存Long类型数据,键[key]，值[value] */
     fun putLong(key: String, value: Long) {
-        mContext?.getSharedPreferences(mName, mMode)?.edit {
+        mContext?.get()?.getSharedPreferences(mName, mMode)?.edit {
             this.putLong(key, value)
         }
     }
 
     /** 获取Long型数据,键[key]，默认值[defValue] */
     fun getLong(key: String, defValue: Long): Long = catch(defValue) {
-        mContext?.getSharedPreferences(mName, mMode)?.getLong(key, defValue) ?: defValue
+        mContext?.get()?.getSharedPreferences(mName, mMode)?.getLong(key, defValue) ?: defValue
     }
 
     /** 保存StringSet类型数据,键[key]，值[value] */
     fun putStringSet(key: String, value: Set<String>) {
-        mContext?.getSharedPreferences(mName, mMode)?.edit {
+        mContext?.get()?.getSharedPreferences(mName, mMode)?.edit {
             this.putStringSet(key, value)
         }
     }
 
     /** 获取StringSet型数据,键[key]，默认值[defValue] */
     fun getStringSet(key: String, defValue: Set<String>): Set<String> = catch(defValue) {
-        mContext?.getSharedPreferences(mName, mMode)?.getStringSet(key, defValue) ?: defValue
+        mContext?.get()?.getSharedPreferences(mName, mMode)?.getStringSet(key, defValue) ?: defValue
     }
 
 
     /** 删除指定键的数据,键[key] */
     fun remove(key: String) {
-        mContext?.getSharedPreferences(mName, mMode)?.edit {
+        mContext?.get()?.getSharedPreferences(mName, mMode)?.edit {
             this.remove(key)
         }
     }
 
     /** 清空整个sp数据 */
     fun clear() {
-        mContext?.getSharedPreferences(mName, mMode)?.edit {
+        mContext?.get()?.getSharedPreferences(mName, mMode)?.edit {
             this.clear()
         }
     }
 
     /** 获取全部的sp数据，没有返回null */
     fun getAll(): Map<String, *>? = catch(null) {
-        mContext?.getSharedPreferences(mName, mMode)?.all
+        mContext?.get()?.getSharedPreferences(mName, mMode)?.all
     }
 
     private fun <T> catch(defValue: T, block: () -> T): T = try {
