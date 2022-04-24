76ad493d9aea63ba96f6a2183a8d9dc402cd5e22
zhouL <lz.mms@foxmail.com>
2022-04-24 15:40:53 +0800

    修复CheckDialog调用BUG

--- app/src/main/java/com/lodz/android/agiledevkt/modules/splash/CheckDialog.kt
+++ app/src/main/java/com/lodz/android/agiledevkt/modules/splash/CheckDialog.kt
@@ -21,36 +21,56 @@ class CheckDialog(context: Context) : BaseDialog(context) {
 
     private val mPositiveBtn by bindView<TextView>(R.id.positive_btn)
 
+    private var mContent = ""
+
+    private var mPositiveName: String = ""
+    private var mPositiveListener: DialogInterface.OnClickListener? = null
+
+
+    private var mNegativeName: String = ""
+    private var mNegativeListener: DialogInterface.OnClickListener? = null
+
     override fun getLayoutId() = R.layout.dialog_check
 
     /** 设置内容文本[content] */
     fun setContentMsg(content: String) {
-        mContentTv.text = content
+        mContent = content
     }
 
     /** 设置内容文本资源id[resId] */
     fun setContentMsg(@StringRes resId: Int) {
-        mContentTv.text = context.getString(resId)
+        mContent = context.getString(resId)
     }
 
     /** 设置确认按钮文本[str]和点击监听器[listener] */
     fun setPositiveText(str: String, listener: DialogInterface.OnClickListener? = null) {
-        setBtn(mPositiveBtn, str, listener)
+        mPositiveName = str
+        mPositiveListener = listener
     }
 
     /** 设置确认按钮文本资源id[resId]和点击监听器[listener] */
     fun setPositiveText(@StringRes resId: Int, listener: DialogInterface.OnClickListener? = null) {
-        setBtn(mPositiveBtn, context.getString(resId), listener)
+        mPositiveName = context.getString(resId)
+        mPositiveListener = listener
     }
 
     /** 设置确认按钮文本[str]和点击监听器[listener] */
     fun setNegativeText(str: String, listener: DialogInterface.OnClickListener? = null) {
-        setBtn(mNegativeBtn, str, listener)
+        mNegativeName = str
+        mNegativeListener = listener
     }
 
     /** 设置确认按钮文本资源id[resId]和点击监听器[listener] */
     fun setNegativeText(@StringRes resId: Int, listener: DialogInterface.OnClickListener? = null) {
-        setBtn(mNegativeBtn, context.getString(resId), listener)
+        mNegativeName = context.getString(resId)
+        mNegativeListener = listener
+    }
+
+    override fun initData() {
+        super.initData()
+        mContentTv.text = mContent
+        setBtn(mPositiveBtn, mPositiveName, mPositiveListener)
+        setBtn(mNegativeBtn, mNegativeName, mNegativeListener)
     }
 
     private fun setBtn(btn: TextView, str: String, listener: DialogInterface.OnClickListener?) {
@@ -62,5 +82,4 @@ class CheckDialog(context: Context) : BaseDialog(context) {
             }
         }
     }
-
 }
