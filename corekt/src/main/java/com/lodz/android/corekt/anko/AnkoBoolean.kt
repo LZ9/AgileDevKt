e23780443d124c572266566c2039656cd62df594
zhouL <lz.mms@foxmail.com>
2023-02-22 10:28:35 +0800

    添加boolean和int互相转换的方法

--- corekt/src/main/java/com/lodz/android/corekt/anko/AnkoBoolean.kt
+++ corekt/src/main/java/com/lodz/android/corekt/anko/AnkoBoolean.kt
@@ -7,3 +7,9 @@ package com.lodz.android.corekt.anko
 
 /** 替代java三目运算符（condition ? a : b）为 condition.then { a } ?: b */
 infix fun <T> Boolean.then(param: () -> T): T? = if (this) param() else null
+
+/** 将布尔型转为Int，0为false，1为true */
+fun Boolean.toInt(): Int = if (this) 1 else 0
+
+/** 将Int转为布尔型，true为1 */
+fun Int.toBoolean(): Boolean = this == 1
